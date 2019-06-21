f957f39ef5feab614d6c3a3b5fee13150acea4ea
EishaMazhar <49692285+EishaMazhar@users.noreply.github.com>
2019-06-21 14:47:13 +0500

    Delete task1(second highest).js

--- task1(second highest).js
+++
@@ -1,5 +0,0 @@
-//  1.Find the second biggest element in the array of numbers. 
-
-let arr=[1,21,7,12,3,2];
-arr.sort(function(a,b){return b-a});
-console.log('Second largest element is : '+ arr[1]);
